a26cef82d5981216e571a4ae9e727029d1a689fa
marcheto <marcheto84@gmail.com>
2016-05-30 22:03:18 +0300

    update

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,20 @@
+package lesson6;
+
 public class Calculator {
-  public int evaluate(String expression) {
-    int sum = 0;
-    for (String summand: expression.split("\\+"))
-      sum += Integer.valueOf(summand);
-    return sum;
-  }
+	public int evaluate(String expression) {
+		int sum = 0;
+		for (String summand : expression.split("\\+"))
+			sum += Integer.valueOf(summand);
+		return sum;
+	}
+
+	public int add(int a, int b) {
+
+		return a + b;
+	}
+
+	public int sub(int i, int j) {
+		// TODO Auto-generated method stub
+		return i - j;
+	}
 }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,11 +1,30 @@
+package lesson6;
+
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CalculatorTest {
-  @Test
-  public void evaluatesExpression() {
-    Calculator calculator = new Calculator();
-    int sum = calculator.evaluate("1+2+3");
-    assertEquals(6, sum);
-  }
+
+	@Test
+	public void evaluatesExpression() {
+	//testing
+		Calculator calculator = new Calculator();
+		int sum = calculator.evaluate("1+2+3");
+		assertEquals(6, sum);
+	}
+
+	@Test
+	public void addSum() {
+		Calculator calculator = new Calculator();
+		int sumAdd = calculator.add(25, 12);
+		assertEquals(37, sumAdd);
+	}
+
+	@Test
+	public void SubSum() {
+		Calculator calculator = new Calculator();
+		int sum = calculator.sub(2, 2);
+		assertEquals(0, sum);
+	}
 }
